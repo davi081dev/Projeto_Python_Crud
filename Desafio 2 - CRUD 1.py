f4b0e36258deacbeb50b2efd5a629bb8a85d1fa9
Michel Serpa <santosserpamichel@gmail.com>
2025-05-18 21:04:46 -0300

    Update Desafio 2 - CRUD 1.py
    Dei uma aprimorada nas opções 1 e 5. Utilizei emojis pra organizar, como Everton fez no código dele, e criei mais funções para a automação. As opções 2, 3 e 4 ainda não foram desenvolvidas pq preciso integrar com o json.

--- Desafio 2 - CRUD 1.py
+++ Desafio 2 - CRUD 1.py
@@ -1,76 +1,152 @@
-# CRUD: Create, Read, Update and Delete
-# Não esquecer de utilizar o arquivo .json
+# Import = bibliotecas/módulos
 
-def main():
+import time # Usei para adicionar o delay em algumas partes "time.sleep(tempo de delay)""
+import sys # Usei para simplificar a saída do sistema "sys.exit()"
 
-    def invalid():
-        print("-"*60)
-        print("OPÇÃO INVÁLIDA!")
-        print("-"*60)
-        main()
+def linha():
+    print("-" * 60)
 
-    def add():
-        print("Deseja algo mais?")
-        add=int(input("\n1 - Sim \n2 - Não\n"))
-        match add:
-            case 1:
-                main()
-            case 2:
-                print("Obrigado!")
+def add():
+    while True:
+        add1 = input("\n❔ Deseja algo mais?\n1️⃣  Sim\n2️⃣  Não\n")
+        if add1 == "1":
+            time.sleep(1)
+            return
+        elif add1 == "2":
+            end()
+            time.sleep(1)
+            sys.exit()
+        else:
+            invalid()
+        
+def end():
+    time.sleep(1)
+    print("👋 Saindo...")
+    time.sleep(1)
+    print("🫂  Obrigado!")
+    time.sleep(1)
+    print("🐶 Au au!")
+    time.sleep(1)
+    
+def invalid():
+    linha()
+    print("❌ OPÇÃO INVÁLIDA! ❌")
+    linha()
+    time.sleep(0.5)
+    print("🔃 Vamos tentar novamente?")
+    time.sleep(0.5)
 
-    def linha():
-        print('-'*60)
-
-
-    print("-"*60)
-    print("Olá! Bem-vindo ao Centro de Adoção Luísa Mel! O que você deseja?" )
-    print("-"*60)
-    print("1 - Adicionar um novo animal;")
-    print("2 - Localizar dados de um animal;")
-    print("3 - Atualizar o cadastro de um animal;")
-    print("4 - Excluir um animal;")
-    print("5 - Solicitar um resgate.")
-    opcao = int(input("Escolha uma das opções acima: "))
-
-    match opcao:
-        case 1:
-           str(input("Qual a espécie do animal? (ex.: gato, cachorro, hamster...) "))
-           str(input("Qual a raça do animal? "))
-           str(input("Qual a idade estimada do animal? "))
-           str(input("O animal possui algum tipo de enfermidade? "))
-           str(input("O animal possui algum tipo de doença perceptível? "))
-           str(input("Deseja inserir mais alguma informação adicional? "))
-           foto=str(input("Deseja inserir alguma foto do animal? "))
-           if foto=='Sim':
-              input("Insira a foto: ")
-              linha()
-              print("Uploading...")
-              linha()
-              print("Cadastro finalizado.")
-              add()
-           else:
-              print("Cadastro finalizado.")
-              add()
-
-
-        case 2:
-            input("Informe o nome do animal que deseja localizar: ")
-            print("EM CONSTRUÇÃO")
-
-        case 3:
-            input("Informe o nome do animal que deseja atualizar: ")
-            print("EM CONSTRUÇÃO")
-
-        case 4:
-            input("Informe o nome do animal que deseja excluir: ")
-            print("EM CONSTRUÇÃO")
-
-        case 5:
-            input("Informe o endereço da ocorrência: ")
-            print("EM CONSTRUÇÃO")
-
-        case __:
+def ask_geral(sim_nao): # Criei uma função geral porque são etapas repetitivas.
+    while True:
+        resp1 = input(sim_nao).strip().lower() # A função ".strip()" remove os espaços em branco
+        if resp1 == "sim":
+            input ("❓ Qual? ")
+            break    
+        elif resp1 == "não" or resp1 == "nao":
+            break
+        else:
             invalid()
 
+def ask_foto(foto): # Tive que criar uma outra função só pra parte da foto porque é uma condicional única.
+    while True:
+        resp2 = input(foto).strip().lower()
+        if resp2 == "sim":
+            print("\n🤳 Insira a foto:") # Vai ser necessário anexar uma foto?
+            time.sleep(1)
+            print("\n⬆️  Uploading...")
+            time.sleep(1)
+            print("\n✅ Informações recebidas. Cadastro finalizado.")
+            linha()
+            time.sleep(0.5)
+            break    
+        elif resp2 == "não" or resp2 == "nao":
+            time.sleep(1)
+            print("\n✅ Informações recebidas. Cadastro finalizado.")
+            time.sleep(0.5)
+            linha()
+            break
+        else:
+            invalid()
 
-main()
+def not_found(): # Enquanto as outras opções do código e a integração com o json não ficam prontas, redirecionei pra essa função
+    time.sleep(1)
+    print("🔍  Buscando...")
+    time.sleep(1)
+    print("😂  Te enganei!")
+    time.sleep(1)
+    print("🚧  Desculpe. Essa parte do código ainda não tá pronta.")
+    time.sleep(1)
+    add()
+
+def info():
+    while True:
+        info = input("\n❓ Deseja adicionar alguma informação complementar?\n1️⃣  Sim\n2️⃣  Não\n")
+        if info == "1":
+            input("\nℹ️  Insira a informação: ")
+            time.sleep(1)
+            print("\n✌️  Obrigado pelas informações. Nossa equipe irá até o local em instantes.")
+            add()
+            return
+        elif info == "2":
+            time.sleep(1)
+            print("\n✌️  Obrigado pelas informações. Nossa equipe irá até o local em instantes.")
+            add()
+            return
+        else:
+            invalid()
+
+def menu_crud1():
+    while True:
+        print("\n🔢 Informe o número correspondente à opção que deseja: 👇")
+        print("1️⃣  Cadastrar animal")
+        print("2️⃣  Localizar dados de um animal")
+        print("3️⃣  Atualizar o cadastro de um animal")
+        print("4️⃣  Excluir um animal")
+        print("5️⃣  Solicitar um resgate")
+        print("0️⃣  Voltar ao menu anterior") # Ainda não integramos todos os CRUDs, então a opção 0 encerra o código.
+        
+        opcao = int(input("☝️  Escolha uma das opções acima: "))
+        match opcao:
+
+            case 1:
+                input("\n📛 Qual o nome do bichinho? ")
+                input("\n🤔 Qual a espécie do bichinho? (ex.: gato, cachorro, hamster...) ")
+                input("\n🔢 Qual a idade estimada do bichinho? ")
+                enf = ask_geral("\n😷 Ele possui algum tipo de enfermidade? ")
+                add_info = ask_geral("\n➕ Deseja inserir mais alguma informação adicional? ") 
+                foto = ask_foto("\n📸 Deseja inserir alguma foto do animal? ")              
+                add()
+
+            case 2:
+                input("\n📛 Informe o nome do animal que deseja localizar: ")
+                not_found()
+
+            case 3:
+                input("\n📛 Informe o nome do animal que deseja atualizar: ")
+                time.sleep(1)
+                not_found()
+
+            case 4:
+                input("\n📛 Informe o nome do animal que deseja excluir: ")
+                not_found()
+        
+            case 5:
+                time.sleep(1)
+                print("\n🚨 Vamos iniciar a solicitação de resgate.\n")
+                time.sleep(1)
+                input("📌 Informe o endereço da ocorrência: ")
+                tel = input("\n📞 Informe um telefone para contato: ")
+                info()
+                        
+            case 0:
+                end()
+                break
+
+            case __:
+                invalid()
+
+linha()
+print("🐾 Olá! Bem-vindo ao Centro de Adoção Luísa Mel! 🐾")
+linha()
+time.sleep(0.5)
+menu_crud1()
